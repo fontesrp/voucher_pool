6a1a9e6516fbf58f71d3c27bc07ecbe5d61cbddb
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-02-12 21:01:01 -0800

    Fix session initializer
    The script now uses the correct path for the ini file and makes the
    declared constants names case-sensitive.

--- config/session.php
+++ config/session.php
@@ -1,9 +1,9 @@
 <?php
 
-$config = parse_ini_file("config.ini", true, INI_SCANNER_TYPED);
+$config = parse_ini_file(__DIR__ . "/config.ini", true, INI_SCANNER_TYPED);
 
 foreach ($config as $section => $params) {
     foreach ($params as $key => $value) {
-        define(strtoupper($section . "_" . $key), $value);
+        define(strtoupper($section . "_" . $key), $value, false);
     }
 }
